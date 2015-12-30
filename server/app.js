888532317c6d4b85bcbd3ddfd4001f2af68f4170
lzcmaro <lzcmaro@163.com>
2015-12-30 23:53:36 +0800

    新增koa-logger, koa-compress

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
-require('babel-core/register');
-
 const path = require('path');
 
 const koa = require('koa');
 const router = require('koa-router')();
-const render = require ('koa-ejs');
+const render = require('koa-ejs');
+const logger = require('koa-logger');
+const compress = require('koa-compress');
 
 const webpack = require('webpack');
 const config = require('../webpack-dev.config');
@@ -19,9 +19,26 @@ app.use(require('koa-webpack-dev-middleware')(compiler, {
 
 app.use(require('koa-webpack-hot-middleware')(compiler));
 
+/**
+ * logger
+ */
+app.use(logger());
 
-//ejs render
-//它会在koa context中加入render()
+/**
+ * respone compress
+ */
+app.use(compress({
+  // filter: function (content_type) {
+  // 	return /text/i.test(content_type)
+  // },
+  threshold: 2048,
+  flush: require('zlib').Z_SYNC_FLUSH
+}));
+
+/**
+ * ejs
+ * 它会在koa context中加入render()
+ */
 render(app, {
   root: path.join(__dirname, '../public'),
   layout: false,
@@ -29,7 +46,6 @@ render(app, {
   debug: true,
   cache: true
 });
-
 
 //routes
 require('./routes')(router);
